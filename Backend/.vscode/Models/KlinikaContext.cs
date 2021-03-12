96bbfa5898c0ff4758c0ce1d65fec147ac45ad6b
SlavkoPetrovic17345 <culinjo@elfak.rs>
2021-03-12 20:29:36 +0100

    master

--- Backend/.vscode/Models/KlinikaContext.cs
+++ Backend/.vscode/Models/KlinikaContext.cs
@@ -1,8 +1,18 @@
 using Microsoft.EntityFremworkCore;
 namespace WebProgramiranje.Backend.Models
 {
+    
     public class KlinikaContext : DbContext
     {
+        public DbSet<Klinike> Klinike {get;set;}
+        public DbSet<Raspored> Klinike {get;set;}
+        public DbSet<Lekar> Klinike {get;set;}
+        
+
+        public KlinikaContext()
+        {
+
+        }
 
     }
 
